10ddc041eaa9e56a543afb30ff9a1769215c5d1d
Fahim090 <FAlSami@grantmatch.com>
2021-12-10 21:52:21 -0500

    component

--- vue-2.0/component/main.js
+++ vue-2.0/component/main.js
@@ -23,14 +23,32 @@ Vue.component('event', {
     props: {
         info:{
             type: Array
+        },
+        len : {
+            type: Number
+        }
+    },
+
+    data(){
+        return{
+            name: '',
+            school: '',
         }
     },
     template:`
         <div>
-            <div  v-for="i in info">
-                <h1>{{i.name}}</h1>
+            <p>Length: {{len}}</p>
+            <div  v-for="i in sortedInfo">
+                <h1>{{i.id}} -- {{i.name}}</h1>
                 <p>{{i.school}}</p>
                 <button @click="$emit('remove', i.id)">Delete</button>
+            </div>
+
+            <div>
+                <h1>Insert Form</h1>
+                <input type='text' placeholder='name...' v-model='name'>
+                <input type='text' placeholder='school...' v-model='school'>
+                <button @click="insert">Add</button>
             </div>
             
         </div>
@@ -39,8 +57,22 @@ Vue.component('event', {
         hello(){
             console.log('Hello Hello!');
             this.$emit('ev');
+        },
+        insert(){
+            console.log('inserting info...',this.name, this.school);
+
+            this.$emit('insert', this.name, this.school);
+
+            this.name = '';
+            this.school = '';
         }
     },
+
+    computed:{
+        sortedInfo(){
+            return this.info.sort((a, b) => a.id - b.id);
+        }
+    }
 })
 
 Vue.component('test', {
@@ -66,6 +98,19 @@ const app1 = new Vue({
         remove(id){
             console.log('removing id.....', id);
             this.info=this.info.filter(e => e.id != id );
+        },
+
+        insert(name, school){
+            latest_id = this.info.sort((b,a) => a.id - b.id)[0].id;
+            new_id = latest_id + 1;
+            const obj = {
+                id: new_id,
+                name : name,
+                school : school
+            };
+
+            this.info.push(obj);
+            //console.log('Final function...', name, school)
         }
     },
 })
