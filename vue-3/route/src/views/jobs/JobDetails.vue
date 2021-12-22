f9f2de1f9bfcd35fdbcfb0822766febb78f813c8
Fahim090 <fahimalsafi@gmail.com>
2021-12-21 20:41:59 -0500

    vue router

---
+++ vue-3/route/src/views/jobs/JobDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+        <h1>Job Details Component</h1>
+        <h3>Job id: {{id}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      id: this.$route.params.id
+    }
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-3/route/src/views/jobs/Jobs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Jobs</h1>
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <router-link :to="{name: 'JobDetails', params: {id:job.id}}">{{ job.name }}</router-link>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jobs",
+  data() {
+    return {
+      jobs: [
+        { id: 1, name: "Vue Developer", description: "Vue developer jobs" },
+        {
+          id: 2,
+          name: "Angular Developer",
+          description: "Angular developer jobs",
+        },
+        { id: 3, name: "React Developer", description: "React developer jobs" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
